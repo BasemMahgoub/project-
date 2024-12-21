a9d73418b53e04ba4d7c19f084bed7171c0d33af
Basem Mahgoub <dr.basemmahgoub@gmail.com>
2024-12-21 21:07:56 +0200

    intialization functions

--- software.cpp
+++ software.cpp
@@ -9,3 +9,10 @@ struct Product {
     int quantity;
     Product* next;
 };
+
+void addProduct(Product*& head, int id, string name, float price,float amount);
+void removeProduct(Product*& head, int id);
+void searchProduct(Product* head, int id);
+void displayCart(Product* head);
+void calculateCheck(Product* head);
+
