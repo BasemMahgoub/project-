fa19aed9d0b4cfe3ce4696bb6739811007597fdd
abdulrhman <gadabdelrahman2005@gmail.com>
2024-12-22 04:38:36 +0200

    adding remove function

--- software.cpp
+++ software.cpp
@@ -40,3 +40,35 @@ void addProduct(Product*& head, int id, string name, float price ,float amount) 
     current->next = newProduct;
     cout << "Product added : " << name << endl;
 }
+
+//remove function
+void removeProduct(Product*& head, int id) {
+    if (head == nullptr) {
+        cout << "Cart is empty. No product to remove.\n";
+        return;
+    }
+
+    Product* current = head;
+    Product* prev = nullptr;
+
+    if (current->id == id) {
+        head = current->next;
+        delete current;
+        cout << "Product with ID " << id << " removed successfully.\n";
+        return;
+    }
+
+    while (current != nullptr && current->id != id) {
+        prev = current;
+        current = current->next;
+    }
+
+    if (current == nullptr) {
+        cout << "Product with ID " << id << " not found in the cart.\n";
+        return;
+    }
+
+    prev->next = current->next;
+    delete current;
+    cout << "Product with ID " << id << " removed successfully.\n";
+}
