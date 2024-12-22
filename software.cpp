e7c76a909c12c0ec0207e44e2074df1849027bb6
Momen-Elmohder <moamenahmed25se@gmail.com>
2024-12-22 02:38:12 +0200

    Added add function

--- software.cpp
+++ software.cpp
@@ -16,3 +16,27 @@ void searchProduct(Product* head, int id);
 void displayCart(Product* head);
 void calculateCheck(Product* head);
 
+//Main
+
+
+
+
+//Add function
+void addProduct(Product*& head, int id, string name, float price ,float amount) {
+
+    Product* newProduct = new Product{ id, name, price, amount, nullptr };
+
+    if (head == nullptr) {
+        head = newProduct;
+        cout << "Product added : " << name << endl;
+        return;
+    }
+
+    Product* current = head;
+    while (current->next != nullptr) {
+        current = current->next; 
+    }
+
+    current->next = newProduct;
+    cout << "Product added : " << name << endl;
+}
